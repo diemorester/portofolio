5533680529ed0946cadc9c529ddb489af9f4e0b2
diemorester <heimrband@gmail.com>
2024-10-01 07:29:22 +0700

    adding float

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import AboutSection from "./components/AboutSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import SkillsSection from "./components/SkillsSection";
 
 export default function App() {
   return (
-    <div className="bg-black min-h-screen font-space-mono">
+    <div className="bg-black min-h-screen font-space-mono cursor-default">
       <Navbar />
       <HeroSection />
       <AboutSection />
+      <SkillsSection />
     </div>
   )
 }
--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,5 +1,15 @@
 export default function SkillsSection() {
     return (
-        <div></div>
+        <div id="skill" className="flex flex-col justify-center min-h-screen px-5 md:px-[137px] gap-2 md:gap-5">
+            <div className="">
+                <p className="text-mono hover:text-white md:text-xl">
+                    &lt;&nbsp;Skills&nbsp; &nbsp;&gt;
+                </p>
+            </div>
+            <div className="flex flex-col md:flex-row">
+                <div>test</div>
+                <div>test</div>
+            </div>
+        </div>
     )
 }
